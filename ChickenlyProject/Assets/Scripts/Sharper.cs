eb87c515e66c0011fc85ad13d77de3c3026b19da
dragonjuve <dragonjuve@hotmail.com>
2017-04-23 11:56:25 +0700

    Fixing UI

---
+++ ChickenlyProject/Assets/Scripts/Sharper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sharper : MonoBehaviour {
+
+    private const int ScaleValue = 10;
+
+    private Text _text;
+    void Start()
+    {
+        _text = gameObject.GetComponent<Text>();
+
+        _text.fontSize = _text.fontSize * ScaleValue;
+
+        _text.transform.localScale = _text.transform.localScale / ScaleValue;
+
+        _text.horizontalOverflow = HorizontalWrapMode.Overflow;
+        _text.verticalOverflow = VerticalWrapMode.Overflow;
+    }
+}
